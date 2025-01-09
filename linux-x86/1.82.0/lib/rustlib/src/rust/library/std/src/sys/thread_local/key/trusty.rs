c89e6b425fa8a87bf51b54beea3c848972d9e81c
Chris Wailes <chriswailes@google.com>
2025-01-09 15:42:08 -0800

    Update Trusty source files in Rust 1.82.0 prebuilt
    Test: TH
    Bug: 369422063
    Change-Id: Ief096aee06c9d57e74b9583fc833393ac0e8c7eb

--- linux-x86/1.82.0/lib/rustlib/src/rust/library/std/src/sys/thread_local/key/trusty.rs
+++ linux-x86/1.82.0/lib/rustlib/src/rust/library/std/src/sys/thread_local/key/trusty.rs
@@ -6,22 +6,24 @@ type Dtor = unsafe extern "C" fn(*mut u8);
 static mut STORAGE: crate::vec::Vec<(*mut u8, Option<Dtor>)> = Vec::new();
 
 #[inline]
-pub unsafe fn create(dtor: Option<Dtor>) -> Key {
-    let key = STORAGE.len();
-    STORAGE.push((ptr::null_mut(), dtor));
-    key
+pub fn create(dtor: Option<Dtor>) -> Key {
+    unsafe {
+        let key = STORAGE.len();
+        STORAGE.push((ptr::null_mut(), dtor));
+        key
+    }
 }
 
 #[inline]
 pub unsafe fn set(key: Key, value: *mut u8) {
-    STORAGE[key].0 = value;
+    unsafe { STORAGE[key].0 = value };
 }
 
 #[inline]
 pub unsafe fn get(key: Key) -> *mut u8 {
-    STORAGE[key].0
+    unsafe { STORAGE[key].0 }
 }
 
 #[inline]
-pub unsafe fn destroy(_key: Key) {
+pub fn destroy(_key: Key) {
 }
