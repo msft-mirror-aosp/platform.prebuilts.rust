eca3f1596ecd032b01be4862aa086d8eed621cbb
Chris Wailes <chriswailes@google.com>
2025-02-06 11:54:02 -0800

    Remove Rust 1.81.0 prebuilts
    Test: TH
    Bug: 384401387
    Change-Id: Iecaf7f99902b86ec36415e158b21ff4821942f6a

--- linux-x86/1.81.0/lib/rustlib/src/rust/library/stdarch/crates/core_arch/src/loongarch64/mod.rs
+++
@@ -1,9 +0,0 @@
-//! `LoongArch` intrinsics
-
-mod lasx;
-mod lsx;
-
-#[unstable(feature = "stdarch_loongarch", issue = "117427")]
-pub use self::lasx::*;
-#[unstable(feature = "stdarch_loongarch", issue = "117427")]
-pub use self::lsx::*;
